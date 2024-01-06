074a6dbaea33d008edd1b60c090a28677385f19f
zjhken <zjhken@outlook.com>
2024-01-06 17:56:03 +0800

    fix

--- install-sync-server.sh
+++ install-sync-server.sh
@@ -11,6 +11,6 @@ cd protoc
 curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
 unzip protoc-3.15.8-linux-x86_64.zip
 ln -s $PWD/bin/protoc /usr/bin/protoc
-
+cd ..
 mkdir -p ./ftl/core-repo/core ./ftl/qt-repo/desktop
 PROTOC=/usr/bin/protoc cargo install --path rslib/sync
